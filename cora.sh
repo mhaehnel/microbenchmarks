5298c96130c1aa86ad83f7004c4e28116aa2683e
Marcus Haehnel <mhaehnel@tudos.org>
2018-09-22 11:18:23 +0200

    Be dynamic for CPU affinity and take online into account

--- cora.sh
+++ cora.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for i in {0..7}; do
-	taskset -c $i "$@" &
+for i in $(seq 0 $(($(nproc --all)-1))); do
+	[ $(< /sys/devices/system/cpu/cpu$i/online) -eq 1 ] && echo $i && taskset -c $i "$@" &
 done
 wait
