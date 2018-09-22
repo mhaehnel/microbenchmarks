6f102bebbc6aec180e794bcb79a229d040815799
Marcus Haehnel <mhaehnel@tudos.org>
2018-09-22 12:54:40 +0200

    Do core affinity right.

--- cora.sh
+++ cora.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-for i in $(seq 0 $(($(nproc --all)-1))); do
-	[ $(< /sys/devices/system/cpu/cpu$i/online) -eq 1 ] && echo $i && taskset -c $i "$@" &
+declare -i MASK=$(( 16#$(taskset -p $$ | cut -f2 -d\: | tr -d ' ') )) POS=0
+while [ $MASK -ne 0 ]; do
+	echo "$POS: $(($MASK%2)) $MASK $(($MASK>>1))"
+	if [ $(( $MASK%2 )) -eq  1 ]; then
+		taskset -c ${POS} "$@" &
+	fi
+	let POS+=1
+	MASK=$(($MASK>>1))
 done
 wait
